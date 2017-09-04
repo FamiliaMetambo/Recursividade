8ec1fa4fda4701113518c5ebf9b1ec27d03d1252
Jose Seie <joseeugenioseie@hotmail.com>
2017-09-04 21:24:41 +0200

    Soma de algarismos concluido

--- src/classes/Soma.java
+++ src/classes/Soma.java
@@ -10,47 +10,47 @@ package classes;
  * @author Mr. UAMBA
  * @author Mr. Jose Fumo
  * @author Mr. Moises Matsinhe
- * 
+ *
  */
-
-
 public class Soma {
-    
-/*metodo para obter a soma de um numero*/
+
+    /*metodo para obter a soma de um numero*/
     public static int soma(int numero) {
-     if(numero == 0)
-         return 0;
-     else
-         System.out.println(numero);
-         return (numero +soma(numero-1));
+        if (numero == 0) {
+            return 0;
+        } else {
+            System.out.println(numero);
+        }
+        return (numero + soma(numero - 1));
     }
-    
+
     /*metodo para obter a factorial de um numero*/
-     public static int factorial(int numero) {
-     if(numero == 0)
-         return 1;
-     else
-         System.out.println(numero);
-         return (numero *factorial(numero-1));
+    public static int factorial(int numero) {
+        if (numero == 0) {
+            return 1;
+        } else {
+            System.out.println(numero);
+        }
+        return (numero * factorial(numero - 1));
     }
-    
-     
-      /*metodo para obter a fibonacci de um numero*/
-     public static int fibonacci(int numero) {
-         //if((numero > 0){
+
+    /*metodo para obter a fibonacci de um numero*/
+    public static int fibonacci(int numero) {
+        //if((numero > 0){
         // throw.NumberFormatException(" o numero nao deve ser negativo");
-     if(numero ==0 || numero == 1)
-         return 1;
-     
-     
-         System.out.println(numero);
-         return (fibonacci(numero-1) *fibonacci(numero-2));
-    
-     }
+        if (numero == 0 || numero == 1) {
+            return 1;
+        }
+
+        System.out.println(numero);
+        return (fibonacci(numero - 1) * fibonacci(numero - 2));
+
+    }
+
     /* programa principal ou teste*/
     public static void main(String[] args) {
-       // System.out.println(soma(10));
+        // System.out.println(soma(10));
         //System.out.println(factorial(10));
         System.out.println(fibonacci(5));
-    } 
+    }
 }
